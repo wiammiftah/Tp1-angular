147ee1a83911ea62521efdcb8688f4b923732cb4
wiam <wiammiftah01@gmail.com>
2024-05-12 01:02:01 +0100

    commit final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  actions : Array<any> =[
-    {title :"Home", "route":"/home", icon : "house"},
-    {title :"Products", "route":"/products", icon : "search"},
-    {title :"New Product", "route":"/newProduct", icon : "safe"}
-  ];
-  currentAction :any;
 
-  setCurrentAction(action: any) {
-    this.currentAction = action;
-
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { NewProductComponent } from './new-product/new-product.component';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { AppErrorsComponent } from './app-errors/app-errors.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotAuthorizedComponent } from './not-autorized/not-autorized.component';
+import {AppHttpInterceptor} from "./services/app-http.interceptor";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProductsComponent,
-    NewProductComponent
+    NewProductComponent,
+    EditProductComponent,
+    AdminTemplateComponent,
+    AppErrorsComponent,
+    DashboardComponent,
+    LoginComponent,
+    NavbarComponent,
+    NotAuthorizedComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule,FormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [
-    provideClientHydration()
+    {provide : HTTP_INTERCEPTORS, useClass : AppHttpInterceptor, multi : true}
   ],
   bootstrap: [AppComponent]
 })
